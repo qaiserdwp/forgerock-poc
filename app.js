5c1fe40926b35fdb4959f85f46e60f2676de814d
Adam Boxall <adam.boxall@engineering.digital.dwp.gov.uk>
2019-07-18 15:12:36 +0100

    Dynamic callback renderering

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const session = require('express-session');
+const logger = require("morgan");
 const nunjucks = require("nunjucks");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
 
-var app = express();
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session({
+  secret: 'f85cdd6fa2',
+  resave: false,
+  saveUninitialized: false,
+}));
 
 app.set("views", path.join(__dirname, "views"));
 nunjucks.configure(
@@ -31,5 +38,7 @@ app.set("view engine", "njk");
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+app.use(authRouter);
 
 module.exports = app;
