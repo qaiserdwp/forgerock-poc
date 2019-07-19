52f235e5cce2dd7f4b672f25522143432a25d3e0
Adam Boxall <adam.boxall@engineering.digital.dwp.gov.uk>
2019-07-19 10:11:05 +0100

    Working end-to-end auth journey

--- auth/callbacks.js
+++ auth/callbacks.js
@@ -1,4 +1,4 @@
-function renderChoice(renderer, {output}, callbackIndex) {
+function renderChoiceCallback(renderer, {output}, callbackIndex) {
   const prompt = output.find(chunk => chunk.name === 'prompt');
   const choices = output.find(chunk => chunk.name === 'choices');
   const defaultChoice = output.find(chunk => chunk.name === 'defaultChoice');
@@ -11,12 +11,35 @@ function renderChoice(renderer, {output}, callbackIndex) {
   });
 }
 
+function renderNameCallback(renderer, {output}, callbackIndex) {
+  const prompt = output.find(chunk => chunk.name === 'prompt');
+
+  return renderer('auth/callbacks/name.njk', {
+    callbackIndex,
+    prompt: prompt.value,
+  });
+}
+
+function renderPasswordCallback(renderer, {output}, callbackIndex) {
+  const prompt = output.find(chunk => chunk.name === 'prompt');
+
+  return renderer('auth/callbacks/password.njk', {
+    callbackIndex,
+    prompt: prompt.value,
+  });
+}
+
 exports.renderCallbacks = function renderCallbacks(renderer, callbacks) {
   return callbacks.map((callback, i) => {
     switch (callback.type) {
       case 'ChoiceCallback':
-        return renderChoice(renderer, callback, i);
+        return renderChoiceCallback(renderer, callback, i);
+      case 'NameCallback':
+        return renderNameCallback(renderer, callback, i);
+      case 'PasswordCallback':
+        return renderPasswordCallback(renderer, callback, i);
       default:
+        console.log('unknown callback', callback);
         throw new Error('unknown callback type');
     }
   });
